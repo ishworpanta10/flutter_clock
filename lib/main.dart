6eab8b69d16cde8137254481fc5140f3422a788e
ishworpanta10 <ishworpanta10@gmail.com>
2020-09-22 19:13:47 +0545

    project setup with required dependencies and setup local notification setting

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Title here"),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+        titleSpacing: 1.2,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Text("Hello I am center"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
